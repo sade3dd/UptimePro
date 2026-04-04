9f3d6f85d9651a416c1a00a13b3b09323784a17c
sade3dd <iiijnin@mkij8jjo08.ip-ddns.com>
2026-04-04 08:45:23 +0800

    Update ui.ts

--- src/ui.ts
+++ src/ui.ts
@@ -8,7 +8,6 @@ export const INDEX_HTML = `
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { 
-            font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             background-color: #000; 
             color: #059669;
         }
